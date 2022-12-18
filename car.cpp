d85868f5124396b7eefd372ee742b41abbd89bb8
Samia <iqrat269@gmail.comgit config --global user.email iqrat269@gmail.com>
2022-12-18 20:41:20 +0500

    message

---
+++ car.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+ main()
+{
+ system ("color 01");
+ cout<<"          &&&&&&&&&&&&&&&&&&&&&                  "<<endl;
+ cout<<"         &    #####   #####    &&                "<<endl;
+ cout<<"       &&    # |   #  #     #    &&               "<<endl;
+ cout<<"      &     ########   ######      &&&&&&        "<<endl;
+ cout<<"   &&&      =          =                 &&       "<<endl;
+ cout<<"  (\\                                       &     "<<endl;
+ cout<<"  (/       &&&                   &&&      @ &   "<<endl;
+ cout<<"  &&     & ** &                & ** &       &  "<<endl;
+ cout<<"    &&&&&*    *&&&&&&&&&&&&&&&&*    * &&&&&&&&  "<<endl;
+ cout<<"           **                    **             "<<endl;
+ }
